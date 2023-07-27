403ef6266855ea948d241b6ef4569be5d15b81ab
stewie-gil <gilliganstewie7@gmail.com>
2023-07-27 08:55:56 +0000

    add

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,13 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.all([userPromise, photoPromise]).then((results) => {
-    return results.map((result) => ({
-      status: 'fulfilled',
-      value: result,
-    }));
-  }).catch((error) => {
-    return [{ status: 'rejected', value: error }];
-  });
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+	res.map((e) => ({
+        status: e.status,
+        value: e.status === 'fulfilled' ? e.value : String(e.reason),
+      }))
+    ));
 }
