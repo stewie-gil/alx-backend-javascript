88bc8de4798056fe5dfe4b47538b535421e97ae4
stewie-gil <gilliganstewie7@gmail.com>
2023-07-27 10:45:09 +0000

    add

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
