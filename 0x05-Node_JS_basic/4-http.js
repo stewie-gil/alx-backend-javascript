f625b7c3277d1814644b68a5e6e8a9f159d98759
stewie-gil <gilliganstewie7@gmail.com>
2023-09-19 17:28:54 -0700

    linter issues

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,7 +5,7 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   // res.statusCode = 200;
-    res.writeHeader(200, {'Content-Type': 'text/plain'});
+  res.writeHeader(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
