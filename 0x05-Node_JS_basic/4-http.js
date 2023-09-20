757d027712530f30cb6102858fc39fb43efaa846
stewie-gil <gilliganstewie7@gmail.com>
2023-09-19 17:25:14 -0700

    app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,9 +4,11 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  // res.statusCode = 200;
+    res.writeHeader(200, {'Content-Type': 'text/plain'});
   res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname);
+
+module.exports = app;
